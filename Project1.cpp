e70bb58428689c6ab8f7eee119930a5d5b6badb2
Steven-Ou <osteve425@gmail.com>
2026-02-18 00:52:45 -0500

    adding in merge sort

--- Project1.cpp
+++ Project1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std; 
 
+void mergeSort(vector<int>& arr, int l, int r){
+    if(l<r){
+        int m = l+(r-l)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+}
 int main(){
-    
+
 }
