90e460dfd00582cf385627069a78ca3aad164b5e
Steven-Ou <osteve425@gmail.com>
2026-02-20 14:26:29 -0500

    Adding in insertionsort full function

--- Project1.cpp
+++ Project1.cpp
@@ -2,7 +2,16 @@
 using namespace std; 
 
 void insertionSort(vector<int>& arr){
-    
+    int n = arr.size();
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
 void merge(vector<int>& arr, int l, int m, int r){
     int n1 = m - l + 1;
