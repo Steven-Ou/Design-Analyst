f518825deb25dfc7ac35fdc14f428103f730593c
Steven-Ou <osteve425@gmail.com>
2026-02-20 01:25:21 -0500

    adding in the merge function

--- Project1.cpp
+++ Project1.cpp
@@ -2,7 +2,19 @@
 using namespace std; 
 
 void merge(vector<int>& arr, int l, int m, int r){
-    
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++]; 
 }
 void mergeSort(vector<int>& arr, int l, int r){
     if(l<r){
