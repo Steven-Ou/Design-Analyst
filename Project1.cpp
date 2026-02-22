4224d3629bc7214ed777adb0fa035de2c643d83b
Steven-Ou <osteve425@gmail.com>
2026-02-22 15:56:00 -0500

    creating an update function

--- Project1.cpp
+++ Project1.cpp
@@ -15,7 +15,11 @@ struct Stats{
     double totalTime = 0, minTime = 999999.0, maxTime = 0;
     long long totalComp = 0, minComp = 9999999999, maxComp = 0;
     long long totalSwaps = 0, minSwaps = 9999999999, maxSwaps = 0;
-}
+
+    void update(double time,  long long comp, long long swaps){
+        
+    }
+};
 void generateRandomArray(vector<int> &arr, int size)
 {
     random_device rd;
