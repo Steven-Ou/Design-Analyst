9c3d9e6c13dfd624353c9e5a6108719ac049ea6d
Steven Ou <osteve425@gmail.com>
2026-04-29 23:27:24 -0400

    the forloops fills in the table using the recursive relations

--- Project2.cpp
+++ Project2.cpp
@@ -11,11 +11,24 @@ double calculateWinProbability(int n, double p, long long &multiplications)
 
     vector<vector<double>> dp(W + 1, vector<double>(W + 1, 0.0));
 
-    for (int j = 1; j <= W; j++) {
+    for (int j = 1; j <= W; j++)
+    {
         dp[0][j] = 1.0;
     }
 
-    for (int i = 1; i <= W; i++) {
+    for (int i = 1; i <= W; i++)
+    {
         dp[i][0] = 0.0;
     }
+    
+    for (int i = 1; i <= W; i++)
+    {
+        for (int j = 1; j <= W; j++)
+        {
+            dp[i][j] = (p * dp[i - 1][j]) + ((1.0 - p) * dp[i][j - 1]);
+            multiplications += 2; // Two multiplications per cell update
+        }
+    }
+
+    return dp[W][W];
 }
