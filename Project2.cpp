a059bb3131027a765048336034f2eeab54587af8
Steven Ou <osteve425@gmail.com>
2026-04-29 23:24:41 -0400

    creating vars that stores num of wins and creating an array  to store gryff, and slyth wins

--- Project2.cpp
+++ Project2.cpp
@@ -4,7 +4,11 @@
 
 using namespace std;
 
-double calculateWinProbability(int n, double p, long long &multiplications) {
+double calculateWinProbability(int n, double p, long long &multiplications)
+{
+    int W = (n / 2) + 1;
+    multiplications = 0;
+
+    vector<vector<double>> dp(W + 1, vector<double>(W + 1, 0.0));
     
 }
-
